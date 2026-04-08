9bb740a9891c1cf88d8e3f604b585ffadda56704
Taylor Yee <tayloryee1@gmail.com>
2026-04-08 13:35:32 -0700

    redis done

--- dAppTest.py
+++ dAppTest.py
@@ -5,18 +5,26 @@ import redis
 
 if __name__ == '__main__':
 
-    app2=dApp()
-    app = dApp()
-    app.redis.flushdb()
-    app2.redis.flushdb()
-    app.new_parm("boi","yeslawd")
-    print("1",flush=True)
-    time.sleep(1)
-    app2.new_parm("boi","uhh why")
-    print("2",flush=True)
-    print(app2.parms["boi"].value,flush=True)
+    appOne = dApp("appOne")
+    appTwo = dApp("appTwo")
+    appOne.redis.flushdb()
+    
+    
+    appOne.new_parm("parmOne","valOne")
+    print(appOne.parms["parmOne"].value,"parm one value")
 
-    print(app.parms["boi"].value,flush=True)
+    appTwo.new_parm("parmOne","valTwo")
+    time.sleep(1)
+    print(appTwo.parms["parmOne"].value)
+
+    appTwo.update_parm("parmOne","valTwo")
+    time.sleep(1)
+
+    print(f"updated appTwo value:{appTwo.parms["parmOne"].value}")
+    print(f"updated appOne value:{appOne.parms["parmOne"].value}")
+    
+    
+
     
 
 
--- myTest.py
+++ myTest.py
@@ -1,17 +1,29 @@
 import argparse
 import redis
+import time
 #Distributed system framework
 class dApp(object):
     
     class Parser(argparse.ArgumentParser):
         pass
 
-    def __init__(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--name',type=str,required=True)
-        self.args = parser.parse_args()
-        self.name = self.args.name
-        
+    class Parm(object): #Represents one key,value locally 
+        def __init__(self,name,value,dAppInstance):
+            self.name = name 
+            self.value = value 
+            self.dAppInstance = dAppInstance
+
+        def update_parm_callback(self,key,new_val):
+            #print(f"{self.name}'s key:{key} changed from {self.value} to {new_val}")
+            self.value = new_val
+            
+
+    def __init__(self,name):
+        # parser = argparse.ArgumentParser()
+        # parser.add_argument('--name',type=str,required=True)
+        # self.args = parser.parse_args()
+        # self.name = self.args.name
+        self.name = name
         self.callbacks = {}#local store key->callback
         self.parms = {}#local store parms
 
@@ -24,24 +36,13 @@ class dApp(object):
         self.redis_listener()#configurers pubsub to listen for key changes and activate handle_redis_event on noti
         self.pubSub.run_in_thread() #listens for key changes in seperate thread
    
-
-    class Parm(object): #Represents one key,value locally 
-        def __init__(self,name,value,dAppInstance):
-            self.name = name 
-            self.value = value 
-            self.dAppInstance = dAppInstance
-
-
-        def update_parm_callback(self,key,val):
-            self.value = val
-        
-            
+         
     def redis_listener(self):
         self.pubSub.psubscribe(**{"__keyspace@0__:*": self.handle_redis_event})
 
     def handle_redis_event(self,message):
         name = message["channel"].split(":")[-1]
-        if name in self.parms: #We need this check because our redis_listener we setup on intilization listens for all key changes to global redis - 
+        if name in self.parms and name in self.callbacks: #We need this check because our redis_listener we setup on intilization listens for all key changes to global redis - 
             #and we only care about key changes that are on keys that exisit in our instance
             global_val = self.redis.get(name)
             for callback in self.callbacks[name]: #callback functions accept (key,val)
@@ -52,7 +53,9 @@ class dApp(object):
             self.callbacks[name] = []
         self.callbacks[name].extend(callbacks)
 
-
+    def log_callback(self,key,val):
+        pass
+   
     def new_parm(self,name,value):#only create parm if it does not exist in global redis storage
         if name in self.parms:#parm already exists locally
             return 
@@ -63,16 +66,18 @@ class dApp(object):
 
         else:#if we created new parm
             self.parms[name] = self.Parm(name,value,self)
-
+        time.sleep(1)
         self.link_callbacks(name,[self.parms[name].update_parm_callback,self.log_callback]) #Link callback
+    
 
-
-    def log_callback(self,key,val):
-        print(f"\n{key} changed to {val}\n",flush=True)
+    def update_parm(self, name, value):
+        if name not in self.parms:
+            return
+        self.redis.set(name, value) #Allow callback to update parm value
     
 
 
-    #def update_key(self,key,value):
+
 
 
         
